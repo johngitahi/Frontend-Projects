51ba74844a3e57ada916e89522c48b79a496aded
John Gitahi <gitahi109@gmail.com>
2022-12-11 12:07:50 +0300

    Complete desktop site

--- intro-section-with-dropdown-nav/script.js
+++ intro-section-with-dropdown-nav/script.js
@@ -1,14 +1,21 @@
 // When the user clicks on the button, toggle between
 // hiding and showing the dropdown content
 
-function dropbtn() {
-    document.getElementById("linkDropdown").classList.toggle("show");
+function dropFeaturesbtn() {
+    document.getElementById("featuresDropdown").classList.toggle("show");
+}
+
+// When the user clicks the Company button, toggle between hiding and showing
+// the dropdown content
+
+function dropCompanybtn() {
+    document.getElementById("companyDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var linkDropdown = document.getElementById("linkDropdown");
+    var linkDropdown = document.getElementById("featuresDropdown");
         if (linkDropdown.classList.contains('show')) {
             linkDropdown.classList.remove('show');
         }
